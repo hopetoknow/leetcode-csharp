50d5aeb46f8b1dd3601e5d592025437a20cec101
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-02-17 21:08:53 +0100

    Add solution for leetcode problem #1470

---
+++ src/_1470_ShuffleTheArray/Solution.cs
@@ -0,0 +1,17 @@
+namespace _1470_ShuffleTheArray;
+
+public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        var ans = new int[2 * n];
+
+        for (int i = 0, j = 0; i < 2 * n; i += 2, j++) 
+        {
+            ans[i] = nums[j];
+            ans[i + 1] = nums[j + n];
+        }
+
+        return ans; 
+    }
+}
