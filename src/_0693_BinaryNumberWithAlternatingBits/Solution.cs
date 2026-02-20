f1b768cdf7d76b0cc02e70a85f85f2bb2d80fa31
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-02-20 18:44:05 +0100

    Add solution for leetcode problem #693

---
+++ src/_0693_BinaryNumberWithAlternatingBits/Solution.cs
@@ -0,0 +1,25 @@
+namespace _0693_BinaryNumberWithAlternatingBits;
+
+public class Solution
+{
+    public bool HasAlternatingBits(int n)
+    {
+        var bits = new List<int>();
+
+        while (n > 0)
+        {
+            bits.Add(n % 2);
+            n /= 2;
+        }
+
+        for (var i = 1; i < bits.Count; i++)
+        {
+            if (bits[i] == bits[i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
