39630ee4ed637f16709fa8e289406e6d3391d931
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-03-18 21:23:59 +0100

    Add second solution (construction) for leetcode problem #1374

--- src/_1374_GenerateAStringWithCharactersThatHaveOddCounts/Solution.cs
+++ src/_1374_GenerateAStringWithCharactersThatHaveOddCounts/Solution.cs
@@ -8,7 +8,9 @@ public class Solution
         {
             return new string('a', n);
         }
-        
+
         return new string('a', n - 1) + "b";
     }
+
+    public string GenerateTheString2(int n) => n % 2 == 1 ? new string('a', n) : new string('a', n - 1) + 'b';
 }
