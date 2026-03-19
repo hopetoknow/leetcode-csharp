3f8afec02c33e16c577d588968ea8d6a7b8b683c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-03-19 23:51:00 +0100

    Add fourth solution (construction) for leetcode problem #1374

--- src/_1374_GenerateAStringWithCharactersThatHaveOddCounts/Solution.cs
+++ src/_1374_GenerateAStringWithCharactersThatHaveOddCounts/Solution.cs
@@ -15,4 +15,18 @@ public class Solution
     public string GenerateTheString2(int n) => n % 2 == 1 ? new string('a', n) : new string('a', n - 1) + 'b';
 
     public string GenerateTheString3(int n) => new string('a', n - 1) + (n % 2 == 1 ? "a" : "b");
+
+    public string GenerateTheString4(int n)
+    {
+        var chars = new char[n];
+        Array.Fill(chars, 'a');
+
+        if (n % 2 == 0)
+        {
+            chars[n - 1] = 'b';
+        }
+
+
+        return new string(chars);
+    }
 }
