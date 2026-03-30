16986160f7a1b262e5819c92a14c743685e25d0a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-03-30 21:36:43 +0200

    Add second solution (bitwise) for leetcode problem #3370

--- src/_3370_SmallestNumberWithAllSetBits/Solution.cs
+++ src/_3370_SmallestNumberWithAllSetBits/Solution.cs
@@ -14,4 +14,16 @@ public class Solution
 
         return x - 1;
     }
+    
+    public int SmallestNumber2(int n)
+    {
+        int x = 1;
+
+        while (x <= n)
+        {
+            x <<= 1;
+        }
+
+        return x - 1;
+    }
 }
