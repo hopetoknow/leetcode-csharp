1e6cd30561cc716495825c28b66d62bd670d9bac
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-04-26 17:08:15 +0200

    Add third solution (upper triangle) for leetcode problem #3898

--- src/_3898_FindTheDegreeOfEachVertex/Solution.cs
+++ src/_3898_FindTheDegreeOfEachVertex/Solution.cs
@@ -19,4 +19,21 @@ public class Solution
     }
 
     public int[] FindDegrees2(int[][] matrix) => matrix.Select(row => row.Sum()).ToArray();
+
+    public int[] FindDegrees3(int[][] matrix)
+    {
+        int n = matrix.Length;
+        var ans = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = i + 1; j < n; j++)
+            {
+                ans[i] += matrix[i][j];
+                ans[j] += matrix[i][j];
+            }
+        }
+
+        return ans;
+    }
 }
