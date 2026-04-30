b9071cd9a1a494425f0799b17ad08bbbc8e44300
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-04-30 20:23:22 +0200

    Add solution (frequency map) for leetcode problem #1207

---
+++ src/_1207_UniqueNumberOfOccurrences/Solution.cs
@@ -0,0 +1,16 @@
+namespace _1207_UniqueNumberOfOccurrences;
+
+public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        var frequencyByNumber = new Dictionary<int, int>();
+
+        foreach (int num in arr)
+        {
+            frequencyByNumber[num] = frequencyByNumber.GetValueOrDefault(num) + 1;
+        }
+
+        return new HashSet<int>(frequencyByNumber.Values).Count == frequencyByNumber.Values.Count;
+    }
+}
