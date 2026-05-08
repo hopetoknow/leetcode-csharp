bf3310eaa587d25a15c5d02ffa10c4cdc4cfee04
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-05-08 21:03:42 +0200

    Add solution (sorting) for leetcode problem #1637

---
+++ src/_1637_WidestVerticalAreaBetweenTwoPointsContainingNoPoints/Solution.cs
@@ -0,0 +1,31 @@
+namespace _1637_WidestVerticalAreaBetweenTwoPointsContainingNoPoints;
+
+public class Solution
+{
+    public int MaxWidthOfVerticalArea(int[][] points)
+    {
+        int n = points.Length;
+        var xs = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            xs[i] = points[i][0];
+        }
+
+        Array.Sort(xs);
+
+        int widestArea = 0;
+
+        for (var i = 1; i < n; i++)
+        {
+            int area = xs[i] - xs[i - 1];
+
+            if (widestArea < area)
+            {
+                widestArea = area;
+            }
+        }
+
+        return widestArea;
+    }
+}
