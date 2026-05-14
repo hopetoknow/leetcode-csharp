acbd8fec49dbc2af1d9a4c71bec71915244e2bba
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-05-14 19:42:30 +0200

    Add third solution (two pointers) for leetcode problem #3925

--- src/_3925_ConcatenateArrayWithReverse/Solution.cs
+++ src/_3925_ConcatenateArrayWithReverse/Solution.cs
@@ -17,4 +17,18 @@ public class Solution
     }
 
     public int[] ConcatWithReverse2(int[] nums) => nums.Concat(nums.Reverse()).ToArray();
+
+    public int[] ConcatWithReverse3(int[] nums)
+    {
+        int n = nums.Length;
+        var ans = new int[2 * n];
+
+        for (int i = 0, j = 2 * n - 1; i < n; i++, j--)
+        {
+            ans[i] = nums[i];
+            ans[j] = nums[i];
+        }
+
+        return ans;
+    }
 }
