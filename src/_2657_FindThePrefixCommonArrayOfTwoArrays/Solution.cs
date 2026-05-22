ee0dc1cd27a7dec3c9ac45d9e691618580ee21fe
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-05-22 22:49:49 +0200

    Add second solution (frequency map) for leetcode problem #2657

--- src/_2657_FindThePrefixCommonArrayOfTwoArrays/Solution.cs
+++ src/_2657_FindThePrefixCommonArrayOfTwoArrays/Solution.cs
@@ -28,4 +28,33 @@ public class Solution
 
         return prefixCommonArray;
     }
+
+    public int[] FindThePrefixCommonArray2(int[] A, int[] B)
+    {
+        int n = A.Length;
+        var frequencyByNumber = new Dictionary<int, int>();
+        int commonCount = 0;
+        var prefixCommonArray = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            frequencyByNumber[A[i]] = frequencyByNumber.GetValueOrDefault(A[i]) + 1;
+
+            if (frequencyByNumber[A[i]] == 2)
+            {
+                commonCount++;
+            }
+
+            frequencyByNumber[B[i]] = frequencyByNumber.GetValueOrDefault(B[i]) + 1;
+
+            if (frequencyByNumber[B[i]] == 2)
+            {
+                commonCount++;
+            }
+
+            prefixCommonArray[i] = commonCount;
+        }
+
+        return prefixCommonArray;
+    }
 }
