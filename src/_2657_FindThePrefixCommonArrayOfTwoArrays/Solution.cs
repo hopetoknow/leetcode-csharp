ede433a8bb94c74464bc35fe1e43541fa901573d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-05-23 19:34:02 +0200

    Add third solution (frequency array) for leetcode problem #2657

--- src/_2657_FindThePrefixCommonArrayOfTwoArrays/Solution.cs
+++ src/_2657_FindThePrefixCommonArrayOfTwoArrays/Solution.cs
@@ -57,4 +57,33 @@ public class Solution
 
         return prefixCommonArray;
     }
+
+    public int[] FindThePrefixCommonArray3(int[] A, int[] B)
+    {
+        int n = A.Length;
+        var frequencies = new int[n + 1];
+        int commonCount = 0;
+        var prefixCommonArray = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            frequencies[A[i]]++;
+
+            if (frequencies[A[i]] == 2)
+            {
+                commonCount++;
+            }
+
+            frequencies[B[i]]++;
+
+            if (frequencies[B[i]] == 2)
+            {
+                commonCount++;
+            }
+
+            prefixCommonArray[i] = commonCount;
+        }
+
+        return prefixCommonArray;
+    }
 }
