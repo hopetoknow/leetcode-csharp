d24f5dbca5428722f626137188126ff581cf64c4
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-05-31 19:31:18 +0200

    Add solution (math) for leetcode problem #3300

---
+++ src/_3300_MinimumElementAfterReplacementWithDigitSum/Solution.cs
@@ -0,0 +1,25 @@
+namespace _3300_MinimumElementAfterReplacementWithDigitSum;
+
+public class Solution
+{
+    public int MinElement(int[] nums)
+    {
+        var min = int.MaxValue;
+
+        foreach (int num in nums)
+        {
+            int digitSum = 0;
+            int n = num;
+
+            while (n > 0)
+            {
+                digitSum += n % 10;
+                n /= 10;
+            }
+
+            min = int.Min(min, digitSum);
+        }
+
+        return min;
+    }
+}
