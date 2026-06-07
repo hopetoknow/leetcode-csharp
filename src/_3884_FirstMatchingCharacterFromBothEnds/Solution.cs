6d55755ce1af37bebb02910d60fa9e4a3d6fd1bf
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-06-07 18:41:57 +0200

    Add solution (two pointers) for leetcode problem #3884

---
+++ src/_3884_FirstMatchingCharacterFromBothEnds/Solution.cs
@@ -0,0 +1,19 @@
+namespace _3884_FirstMatchingCharacterFromBothEnds;
+
+public class Solution
+{
+    public int FirstMatchingIndex(string s)
+    {
+        int n = s.Length;
+
+        for (int i = 0; i < (n + 1) / 2; i++)
+        {
+            if (s[i] == s[n - i - 1])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
