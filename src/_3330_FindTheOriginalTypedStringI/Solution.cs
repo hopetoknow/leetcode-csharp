5d786898b868b06d212f6f234fcc670cb14f87d4
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-06-12 12:19:44 +0200

    Add solution (one pass) for leetcode problem #3330

---
+++ src/_3330_FindTheOriginalTypedStringI/Solution.cs
@@ -0,0 +1,19 @@
+namespace _3330_FindTheOriginalTypedStringI;
+
+public class Solution
+{
+    public int PossibleStringCount(string word)
+    {
+        int count = 1;
+
+        for (int i = 1; i < word.Length; i++)
+        {
+            if (word[i] == word[i - 1])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
