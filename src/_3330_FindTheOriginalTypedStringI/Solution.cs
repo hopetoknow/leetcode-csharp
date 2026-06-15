9668ec92d85a626a63600def024d5c200c051b32
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-06-15 22:25:14 +0200

    Add fourth solution (LINQ) for leetcode problem #3330

--- src/_3330_FindTheOriginalTypedStringI/Solution.cs
+++ src/_3330_FindTheOriginalTypedStringI/Solution.cs
@@ -38,4 +38,9 @@ public class Solution
         return 1 + Enumerable.Range(1, word.Length - 1)
             .Count(i => word[i] == word[i - 1]);
     }
+
+    public int PossibleStringCount4(string word)
+    {
+        return 1 + word.Zip(word.Skip(1), (a, b) => a == b ? 1 : 0).Sum();
+    }
 }
