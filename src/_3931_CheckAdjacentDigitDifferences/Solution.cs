0e7ec4dc0f37ca7db19f272232056c1e7794ff3a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-06-24 22:52:26 +0200

    Add third solution (LINQ zip) for leetcode problem #3931

--- src/_3931_CheckAdjacentDigitDifferences/Solution.cs
+++ src/_3931_CheckAdjacentDigitDifferences/Solution.cs
@@ -3,7 +3,7 @@ namespace _3931_CheckAdjacentDigitDifferences;
 public class Solution
 {
     public bool IsAdjacentDiffAtMostTwo(string s)
-    {        
+    {
         for (var i = 1; i < s.Length; i++)
         {
             if (Math.Abs(s[i] - s[i - 1]) > 2)
@@ -14,8 +14,14 @@ public class Solution
 
         return true;
     }
-    
-    public bool IsAdjacentDiffAtMostTwo2(string s) {
+
+    public bool IsAdjacentDiffAtMostTwo2(string s)
+    {
         return Enumerable.Range(0, s.Length - 1).All(i => Math.Abs(s[i] - s[i + 1]) <= 2);
+    }
+
+    public bool IsAdjacentDiffAtMostTwo3(string s)
+    {
+        return s.Zip(s.Skip(1)).All(p => Math.Abs(p.First - p.Second) <= 2);
     }
 }
