232b08653b236f707002572bd5b75b2adf010980
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-07-16 22:47:09 +0200

    Add second solution (coordinate parity) for leetcode problem #1812

--- src/_1812_DetermineColorOfAChessboardSquare/Solution.cs
+++ src/_1812_DetermineColorOfAChessboardSquare/Solution.cs
@@ -6,4 +6,12 @@ public class Solution
     {
         return (coordinates[0] + coordinates[1]) % 2 != 0;
     }
+
+    public bool SquareIsWhite2(string coordinates)
+    {
+        int column = coordinates[0] - 'a';
+        int row = coordinates[1] - '1';
+
+        return (column % 2) != (row % 2);
+    }
 }
