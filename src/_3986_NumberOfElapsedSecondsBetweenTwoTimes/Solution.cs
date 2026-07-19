d67909f182c017237a27b0bb533a97408750546d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-07-19 11:55:33 +0200

    Add solution (parsing) for leetcode problem #3986

---
+++ src/_3986_NumberOfElapsedSecondsBetweenTwoTimes/Solution.cs
@@ -0,0 +1,18 @@
+namespace _3986_NumberOfElapsedSecondsBetweenTwoTimes;
+
+public class Solution
+{
+    public int SecondsBetweenTimes(string startTime, string endTime)
+    {
+        return ToSeconds(endTime) - ToSeconds(startTime);
+    }
+
+    private int ToSeconds(string time)
+    {
+        int h = int.Parse(time.Substring(0, 2));
+        int m = int.Parse(time.Substring(3, 2));
+        int s = int.Parse(time.Substring(6, 2));
+
+        return h * 3600 + m * 60 + s;
+    }
+}
